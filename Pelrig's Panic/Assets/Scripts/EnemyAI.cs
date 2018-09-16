47311ded504f00b6affa0e91ad56b5b4657d012b
bxb5733 <bxb5733@IGM2550-16>
2018-09-15 21:09:24 -0400

    Enemies destroy coins

--- Pelrig's Panic/Assets/Scripts/EnemyAI.cs
+++ Pelrig's Panic/Assets/Scripts/EnemyAI.cs
@@ -15,8 +15,33 @@ public class EnemyAI : MonoBehaviour {
         time += Time.deltaTime;
 		if (time > 3.0f)
         {
+            time = 0.0f;
             //Move
             //For now, heuristic movement to closest unit
+            CheckCoinDestroy();
         }
-	}
+    }
+
+    //To verify
+    private void CheckCoinDestroy()
+    {
+        for (int i = 0; i < Board.currentNumCoins; i++)
+        {
+            //check for a hit
+            if (transform.GetComponent<Piece>().rowPosition == Board.allCoins[i].rowPosition && 
+                transform.GetComponent<Piece>().colPosition == Board.allCoins[i].colPosition)
+            {
+                Board.allCoins[i].GetPiece().transform.position = new Vector3(10000, 10000, 0.0f);
+                Board.allCoins[i].rowPosition = -5;
+                Board.allCoins[i].colPosition = -5;
+                for (int j = i; j < Board.allCoins.Length - 2; j++)
+                {
+                    Board.allCoins[j] = Board.allCoins[j + 1];
+                }
+                //in every situation where the number of coins increases or decreases, adjust the timeToWait
+                Board.currentNumCoins--;
+                Board.timeToWait /= Board.approxGoldenRatio;
+            }
+        }
+    }
 }
