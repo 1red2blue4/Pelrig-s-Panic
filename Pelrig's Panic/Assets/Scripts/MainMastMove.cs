2ef847dda96c1d83b7e90fa14ab7f7485739a283
Kaustabh Dutta (RIT Student) <kxd5954@ad.rit.edu>
2018-09-24 21:43:22 -0400

    Main mast movement (under work)

---
+++ Pelrig's Panic/Assets/Scripts/MainMastMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMastMove : MonoBehaviour {
+
+    GameObject mainMast;
+   
+
+	// Use this for initialization
+	void Start () {
+        mainMast = GameObject.FindWithTag("MainMast");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveMastWhenHit()
+    {
+
+    }
+}
