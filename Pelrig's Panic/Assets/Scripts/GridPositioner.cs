313de355d1661c9f7793b9ab6933e4bcfa753cc6
Mark Obeldobel <markobeldobel@yahoo.com>
2018-09-27 11:30:11 -0400

    Loading Scene set up.

--- Pelrig's Panic/Assets/Scripts/GridPositioner.cs
+++ Pelrig's Panic/Assets/Scripts/GridPositioner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GridPositioner : MonoBehaviour {
 
+    public Vector3 startPos;
     public float difference;
     public float speed;
 
@@ -13,7 +14,10 @@ public class GridPositioner : MonoBehaviour {
         Ray rayResult = new Ray(transform.position, new Vector3(0.0f, 0.0f, 1.0f));
         if (Physics.Raycast(rayResult, out hit, Mathf.Infinity))
         {
+            Debug.Log(hit.collider.name);
+            startPos = transform.position;
             float zChange = hit.collider.transform.position.z - transform.position.z;
+            Debug.Log(zChange);
             difference = zChange;
             speed = 0;
         }
@@ -21,11 +25,18 @@ public class GridPositioner : MonoBehaviour {
 
     public void GuideToObjectBeneath(float distanceToMove)
     {
+        //if it's still falling, fall
         if (difference > 0)
         {
-            transform.position += new Vector3(0.0f, 0.0f, distanceToMove);
+            transform.position += new Vector3(0.0f, 0.0f, distanceToMove + speed);
             difference -= distanceToMove + speed;
             speed += distanceToMove * 0.05f;
+        }
+        //if it's done falling, adjust its position to where it should land
+        else
+        {
+            transform.position += new Vector3(0.0f, 0.0f, difference);
+            difference = 0;
         }
     }
 }
