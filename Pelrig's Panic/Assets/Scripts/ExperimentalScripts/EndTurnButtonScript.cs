0c78cf8d6d90dd6e6005b077501612d21ecdda5d
na7560 <na7560@IGM2550-19>
2018-10-19 22:02:59 -0400

    Added UI Interface for Movement and Energy. Added UI Button EndTurn

---
+++ Pelrig's Panic/Assets/Scripts/ExperimentalScripts/EndTurnButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTurnButtonScript : MonoBehaviour
+{
+    //When EndTurn button is presed
+    public void OnEndTurn()
+    {
+        Debug.Log("The Player has finished his Turn");
+    }
+    
+}
