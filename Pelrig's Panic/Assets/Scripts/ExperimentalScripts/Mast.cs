54f790a1c905e239d0083f098614490ee76ba658
burhanuddinmb <burhanuddin.mb@gmail.com>
2018-11-13 11:14:41 -0500

    Clean up

--- Pelrig's Panic/Assets/Scripts/ExperimentalScripts/Mast.cs
+++ Pelrig's Panic/Assets/Scripts/ExperimentalScripts/Mast.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 
 public class Mast : MonoBehaviour {
 
-    Color inputMaterial;
-    Color invisibleColor;
-    private void Start()
-    {
-        inputMaterial = GetComponent<Renderer>().material.color;
-        invisibleColor = Color.white;
-        invisibleColor.a = 0;
-    }
-
     private void OnMouseEnter()
     {
-        Debug.Log("Enter");
         gameObject.GetComponent<MeshRenderer>().enabled = false;
     }
 
     private void OnMouseExit()
     {
         gameObject.GetComponent<MeshRenderer>().enabled = true;
-        GetComponent<Renderer>().material.SetColor("_Color", inputMaterial);
     }
 }
