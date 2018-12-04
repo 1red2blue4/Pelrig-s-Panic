5d97f915bbca004750e2d9741024f80551c5fe04
ns7767 <ns7767@IGM2550-17>
2018-12-04 10:40:12 -0500

    HUD display

--- Pelrig's Panic/Assets/HUDTextScript.cs
+++ Pelrig's Panic/Assets/HUDTextScript.cs
@@ -7,6 +7,12 @@ public class HUDTextScript : MonoBehaviour
 {
     [SerializeField] Text[] presenceTextArr;
     [SerializeField] Text[] resistTextArr;
+
+    private int healthValue;
+    private int attackValue;
+
+    private bool temp;
+
     
     // Use this for initialization
     void Start ()
@@ -16,17 +22,36 @@ public class HUDTextScript : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update ()
-    {
+    {       
         UpdateText();
+
+        
     }
 
 
     void UpdateText()
     {
-        for (int i = 0; i < Board.possibleMoveableChars.Length; i++)
+       
+        foreach (var item in Board.possibleMoveableChars)
         {
-            presenceTextArr[i].text = Board.possibleMoveableChars[i].presenceValue.ToString();
-            resistTextArr[i].text = Board.possibleMoveableChars[i].resistanceValue.ToString();            
+            for (int i = 0; i < Board.possibleMoveableChars.Length; i++)
+            {
+
+                //temp = Board.possibleMoveableChars[i].thePiece.GetComponent<Stats>().canAttack;
+                 
+                //if(temp)
+                //{
+                //    if (Board.possibleMoveableChars[i].thePiece.name == gameObject.transform.GetChild(i).name)
+                //    {
+                        healthValue = Board.possibleMoveableChars[i].thePiece.GetComponent<Stats>().health;
+                        presenceTextArr[i].text = healthValue.ToString();
+
+                        attackValue = Board.possibleMoveableChars[i].thePiece.GetComponent<Stats>().damage;
+                        resistTextArr[i].text = attackValue.ToString();
+                //    }
+                //}
+                   
+            }
         }
     }
 }
