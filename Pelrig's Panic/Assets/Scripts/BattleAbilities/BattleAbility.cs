273e98998dc1b6651f1ce96f6e3738fc60fa69dd
na7560 <na7560@IGM2550-19>
2019-01-16 15:20:25 -0500

    Preparing Buttons for IndividualCharacter Abilities

---
+++ Pelrig's Panic/Assets/Scripts/BattleAbilities/BattleAbility.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class BattleAbility : MonoBehaviour
+{
+    public Texture icon;
+    public string abilityName;
+    public string abilityDescription;
+    public int id;
+    
+}
---
+++ Pelrig's Panic/Assets/Scripts/BattleAbilities/BattleAbilityList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleAbilityList : MonoBehaviour
+{
+    [SerializeField] public BattleAbility[] childAbilities;
+    [SerializeField] public int characterIdNumber;
+}
