9ea317ca415e3d8a7f8ecd16bb3c2ce291c7df3c
na7560 <na7560@IGM2550-19>
2019-01-22 11:39:25 -0500

    Created custome turnCooldown script test in PirateUI Scene. Preparing test on new current game scene

---
+++ Pelrig's Panic/Assets/Scripts/BattleAbilities/TurnCooldownObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnCooldownObject : MonoBehaviour
+{
+    public int turn, maxTurn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxTurn = 4;
+        turn = maxTurn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EndTurnButtonScript.isButtonPressed == true && turn > 0)
+        {
+            turn--;
+        }
+        else if (turn == 0 && EndTurnButtonScript.isButtonPressed == true)
+        {
+            turn = maxTurn;
+        }
+
+        Debug.Log(turn);
+    }
+}
