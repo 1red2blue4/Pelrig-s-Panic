12acdb36c4d1686bf80be8c219b1c64dde848136
Mark Obeldobel <markobeldobel@yahoo.com>
2019-01-22 11:38:04 -0500

    Cleaned up some dialogue code.

--- Pelrig's Panic/Assets/Scripts/DialogueManager/PanelManager.cs
+++ Pelrig's Panic/Assets/Scripts/DialogueManager/PanelManager.cs
@@ -14,6 +14,7 @@ public class PanelManager : MonoBehaviour, DialogueStateManager
     public static bool isPressed;
     public static bool playerControlsLocked;
     public static int countDialogueLength;
+    [SerializeField] private int maxCountDialogueLength;
 
     [SerializeField]
     private GameObject dialoguePanel;
@@ -50,25 +51,28 @@ public class PanelManager : MonoBehaviour, DialogueStateManager
             }
             BootSequence();                
         }
-        if (countDialogueLength >= 18)
+        if (Input.GetKey(KeyCode.P) || countDialogueLength >= currentEvent.dialogues.Count)
         {
-            dialoguePanel.SetActive(false);
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                dialoguePanel.SetActive(false);
+                countDialogueLength = 0;
+                stepIndex = 0;
+                playerControlsLocked = true;
+                SceneManager.LoadScene("PirateshipScene");
+            }
+
+            if (SceneManager.GetActiveScene().buildIndex == 2)
+            {
+                dialoguePanel.SetActive(false);
+                playerControlsLocked = true;
+            }
+            else
+            {
+                playerControlsLocked = false;
+            }
         }
-        if (SceneManager.GetActiveScene().buildIndex == 1 && (Input.GetKey(KeyCode.P) || countDialogueLength >= 16))
-        { 
-            countDialogueLength = 0;
-            stepIndex = 0;
-            playerControlsLocked = true;
-            SceneManager.LoadScene("PirateshipScene");
-        }
-        if (SceneManager.GetActiveScene().buildIndex == 2 && countDialogueLength <= 17)
-        {           
-            playerControlsLocked = true;
-        }
-        else
-        { 
-            playerControlsLocked = false;
-        }
+        
     }
 
     private void InitiziliasePanels()
