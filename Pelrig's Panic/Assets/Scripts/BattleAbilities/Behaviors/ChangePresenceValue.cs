983a354949e00b11730f92081d40b5729db1494b
na7560 <na7560@IGM2550-19>
2019-02-04 18:07:23 -0500

    Working on Presence CHanging Abilities template

---
+++ Pelrig's Panic/Assets/Scripts/BattleAbilities/Behaviors/ChangePresenceValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangePresenceValue : AbilityBehaviors
+{
+    private const string name = "ChangePresenceValue";
+    private const string description = "Changes Presence Value";
+    private const BehaviorStartTimes startTime = BehaviorStartTimes.Beginning;
+    // private const Sprite icon = Resources.Load();
+
+    private int presenceChangeValue;
+
+    public ChangePresenceValue(int presenceValue) : base(new BasicObjectInformation(name, description), startTime)
+    {
+
+    }
+
+    public int presnceChangeValue
+    {
+        get { return presenceChangeValue; }
+    }
+}
